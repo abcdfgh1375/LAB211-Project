10cad6b14cbf3d09b92ed445327dea540f9d2c8e
Aurora Cao <136096879+abcdfgh1375@users.noreply.github.com>
2024-01-12 20:38:50 +0700

    Delete CandidateManagement directory

--- CandidateManagement/Applicance.java
+++
@@ -1,6 +0,0 @@
-public class Applicance {
-    public static void main(String[] args) {
-        System.out.println("null");
-        System.out.println("no");
-    }
-}
--- CandidateManagement/Controller/CandidateController.java
+++
@@ -1,6 +0,0 @@
-public class CandidateController {
-    public static void main(String[] args) {
-        System.out.println("null");
- System.out.println("lol");
-}
-}
