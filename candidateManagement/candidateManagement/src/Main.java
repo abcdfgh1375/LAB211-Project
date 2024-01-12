deab1cd9caa252cce02a9d6a33000c7fd44581f7
Aurora Cao <136096879+abcdfgh1375@users.noreply.github.com>
2024-01-12 20:39:23 +0700

    Delete candidateManagement directory

--- candidateManagement/candidateManagement/src/Main.java
+++
@@ -1,7 +0,0 @@
-import View.CandidatesViewManagement;
-public class Main {
-    public static void main(String[] args) {
-        CandidatesViewManagement manage = new CandidatesViewManagement();
-        manage.run();
-    }
-}
