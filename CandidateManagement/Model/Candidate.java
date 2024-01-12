37676522b58752f91910c01629ac0d209d10b353
Aurora Cao <136096879+abcdfgh1375@users.noreply.github.com>
2024-01-12 01:23:48 +0000

    he

--- CandidateManagement/Model/Candidate.java
+++ CandidateManagement/Model/Candidate.java
@@ -1,4 +1,15 @@
 package Model;
-class Candidate{
+
+import java.sql.Date;
+
+abstract class Candidate{
+    private String candidateID;
+    private String firstName;
+    private String lastName;
+    private Date birthDay;
+    private String address;
+    private int phone;
+    private String email;
+    private String candidateType;
     
 }
---
+++ CandidateManagement/View/Menu.java
@@ -0,0 +1,34 @@
+package View;
+
+import java.util.List;
+import java.util.Scanner;
+
+public abstract class Menu {
+    private String title;
+    private List<String> list;
+    private Element e = new Element();
+    public Menu(String title, List<String> list) {
+        this.title = title;
+        this.list = list;
+    }
+    public void display(){
+        System.out.println(this.title);
+        for(int i = 0; i < list.size(); i++){
+            System.out.println((i+1) + ". " + list.get(i));
+        }
+    }
+    public int getChoice(){
+        return e.inputPositiveInt("Enter selection");
+    }
+    
+    public abstract void execute(int choice);
+    public void run(){
+        int choice;
+        do{
+        display();
+        choice = getChoice();
+        execute(choice);
+        }while(choice > 0 && choice < list.size());
+    }
+   
+}
