9a267973d3640e6dc52ed1a2d7756286e4ffa582
Aurora Cao <136096879+abcdfgh1375@users.noreply.github.com>
2024-02-28 10:43:12 +0700

    thêm exception

--- LABstudentManagement/src/common/librarySt.java
+++ LABstudentManagement/src/common/librarySt.java
@@ -6,7 +6,11 @@ import java.util.Collections;
 import java.util.List;
 
 public class librarySt {
-    public List<Student> sortStudent(List<Student> a){
+    public List<Student> sortStudent(List<Student> a) throws Exception{
+        if(a.isEmpty()){
+            System.out.println("List is empty!");
+            return null;
+        }
         Collections.sort(a);
         return a;
     }
